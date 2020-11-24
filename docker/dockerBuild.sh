316d8add1ee9ee4a002d0e22a0adf69977078241
Steve Goldstein <sgoldstein@wisc.edu>
2020-11-23 20:35:19 -0600

    install openxlsx

--- docker/dockerBuild.sh
+++ docker/dockerBuild.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 ## Build docker image;
 
+ver=$1
 ## The first builds all the shared layers
-nohup docker build -t steveg53703/epinow2:cran_noAVX2 -f Dockerfile.cran . 1> dockerBuild.cran.out 2> dockerBuild.cran.err
+nohup docker build -t steveg53703/epinow2:cran -f Dockerfile.cran . \
+    1> out/dockerBuild.cran.ver$ver.out \
+    2> out/dockerBuild.cran.ver$ver.err
 
 ## The last two just need to rebuild the final layer that installs EpiNow2
-nohup docker build -t steveg53703/epinow2:github.1.0.0_noAVX2 -f Dockerfile.github.1.0.0 . 1> dockerBuild.github.1.0.0.out 2> dockerBuild.github.1.0.0.err &
-nohup docker build -t steveg53703/epinow2:github.1.1.0_noAVX2 -f Dockerfile.github.1.1.0 . 1> dockerBuild.github.1.1.0.out 2> dockerBuild.github.1.1.0.err &
+nohup docker build -t steveg53703/epinow2:github.1.0.0 -f Dockerfile.github.1.0.0 . \
+    1> out/dockerBuild.github.1.0.0.ver$ver.out \
+    2> out/dockerBuild.github.1.0.0.ver$ver.err
+
+nohup docker build -t steveg53703/epinow2:github.1.1.0 -f Dockerfile.github.1.1.0 . \
+    1> out/dockerBuild.github.1.1.0.ver$ver.out \
+    2> out/dockerBuild.github.1.1.0.ver$ver.err
